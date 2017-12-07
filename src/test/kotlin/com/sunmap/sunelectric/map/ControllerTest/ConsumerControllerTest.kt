ad4c5ec259da4be0672f2fe46c9f1203e1b3155f
Ningyuan Gao <ning@NingyuandeMacBook-Pro.local>
2017-12-07 15:59:16 +0800

    Add consumer controller endpoint

--- src/test/kotlin/com/sunmap/sunelectric/map/ControllerTest/ConsumerControllerTest.kt
+++ src/test/kotlin/com/sunmap/sunelectric/map/ControllerTest/ConsumerControllerTest.kt
@@ -1,5 +1,8 @@
 package com.sunmap.sunelectric.map.ControllerTest
 
+import com.sunmap.sunelectric.map.repositories.ConsumerAccountRepository
+import com.sunmap.sunelectric.map.utils.ConsumerAccountBuilder
+import org.hamcrest.Matchers
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,12 +22,35 @@ class ConsumerControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
+    @Autowired
+    lateinit var consumerAccountRepository: ConsumerAccountRepository
+
+
     @Test
     fun getAllConsumers() {
+        val consumerAccountOne = consumerAccountRepository.save(ConsumerAccountBuilder().default())
+        val consumerAccountTwo = consumerAccountRepository.save(ConsumerAccountBuilder(address = "290 Robinson").default())
 
         mockMvc
                 .perform(MockMvcRequestBuilders.get("/consumer")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].address", Matchers.`is`(consumerAccountOne.address)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].solarPlan", Matchers.`is`(consumerAccountOne.solarPlan!!.toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].address", Matchers.`is`(consumerAccountTwo.address)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].solarPlan", Matchers.`is`(consumerAccountTwo.solarPlan!!.toString())))
+    }
+
+    @Test
+    fun getConsumerByAddress() {
+        val consumerAccount = consumerAccountRepository.save(ConsumerAccountBuilder().default())
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/consumer/${consumerAccount.address}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.address", Matchers.`is`(consumerAccount.address)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.solarPlan", Matchers.`is`(consumerAccount.solarPlan!!.toString())))
+
     }
 }
